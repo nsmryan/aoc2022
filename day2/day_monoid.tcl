542917fb23282bd0c3a3f955183d565d6313a88e
Noah Ryan <nsmryan@gmail.com>
2022-12-02 14:20:43 -0500

    More golfed

--- day2/day_monoid.tcl
+++ day2/day_monoid.tcl
@@ -1,11 +1,7 @@
 foreach { a b } [read [open input.txt r]] {
     set index [dict get [list X 0 Y 1 Z 2] $b]
     incr total1 [expr $index + 1 + 3 * (($index + 2 * [dict get [list A 0 B 1 C 2] $a] + 1) % 3)]
-}
-foreach { a b } [read [open input.txt r]] {
     set outcome [dict get [list X 0 Y 1 Z 2] $b]
-    set oppIndex [dict get [list A 0 B 1 C 2] $a]
-    incr total2 [expr [expr ($oppIndex + $outcome + 2) % 3] + 1 + 3 * $outcome]
+    incr total2 [expr [expr ([dict get [list A 0 B 1 C 2] $a] + $outcome + 2) % 3] + 1 + 3 * $outcome]
 }
-puts $total1
-puts $total2
+puts "Part 1: $total1\nPart 2: $total2"
