e7a31b501a1c0925c18f63d97abd376aae011b2d
Noah Ryan <nsmryan@gmail.com>
2022-12-09 10:50:57 -0500

    Day 9 TCL solution for part 2

--- day9/day.tcl
+++ day9/day.tcl
@@ -9,10 +9,10 @@ set offsets [list R [list 1 0] L [list -1 0] U [list 0 -1] D [list 0 1]]
 set total1 0
 set total2 0
 
-set hx 0
-set hy 0
-set tx 0
-set ty 0
+for { set i 0 } { $i < 10 } { incr i } {
+    set knotsX($i) 0
+    set knotsY($i) 0
+}
 
 proc distance { x0 y0 x1 y1 } {
     return [expr max(abs($x0 - $x1), abs($y0 - $y1))]
@@ -28,32 +28,72 @@ proc sign { value } {
     }
 }
 
-struct::set include visited ($tx,$ty)
+proc printRope { } {
+    global knotsX knotsY
+    set maxX 5
+    set maxY 5
+    set current [list]
+    for { set i 0 } { $i < 10 } { incr i } {
+        dict set current $knotsX($i),$knotsY($i) $i
+        #puts "$knotsX($i),$knotsY($i) $i"
+        #set maxX [expr max($maxX, $knotsX($i))]
+        #set maxY [expr max($maxY, $knotsY($i))]
+    }
+    for { set y -$maxY } { $y < $maxY } { incr y } {
+        for { set x -$maxX } { $x < $maxX } { incr x } {
+            if { [dict exists $current $x,$y] } {
+                puts -nonewline [dict get $current $x,$y]
+            } else {
+                puts -nonewline .
+            }
+        }
+        puts ""
+    }
+    puts "___________________"
+}
+
+struct::set include visited (0,0)
 
 foreach line $input {
     if { $line == "" } continue
     lassign $line dir amount
 
-    lassign [dict get $offsets $dir] xOffset yOffset
 
     for { set index 0 } { $index < $amount } { incr index } {
-        incr hx $xOffset
-        incr hy $yOffset
+        #printRope
+        lassign [dict get $offsets $dir] xOffset yOffset
+        for { set i 0 } { $i < 10 } { incr i } {
 
-        if { [distance $hx $hy $tx $ty] > 1 } {
-            set txOffset [sign [expr $hx - $tx]]
-            set tyOffset [sign [expr $hy - $ty]]
+            incr knotsX($i) $xOffset
+            incr knotsY($i) $yOffset
 
-            incr tx $txOffset
-            incr ty $tyOffset
+            if { $i > 0 } {
+                set lastI [expr $i - 1]
+                if { [distance $knotsX($lastI) $knotsY($lastI) $knotsX($i) $knotsY($i)] > 1 } {
+                    set txOffset [sign [expr $knotsX($lastI) - $knotsX($i)]]
+                    set tyOffset [sign [expr $knotsY($lastI) - $knotsY($i)]]
 
-            struct::set include visited ($tx,$ty)
+                    incr knotsX($i) $txOffset
+                    incr knotsY($i) $tyOffset
+
+                }
+            }
+
+            if { $i != 9 } {
+                set nextI [expr $i + 1]
+                if { [distance $knotsX($i) $knotsY($i) $knotsX($nextI) $knotsY($nextI)] > 1 } {
+                    set xOffset [sign [expr $knotsX($i) - $knotsX($nextI)]]
+                    set yOffset [sign [expr $knotsY($i) - $knotsY($nextI)]]
+                } else {
+                    # no need to continue if distance is not enough to keep moving rope.
+                    break
+                }
+            }
         }
+        struct::set include visited ($knotsX(9),$knotsY(9))
     }
 
 }
 
 set total1 [struct::set size $visited]
-puts "Part 1: $total1"
-
-puts "Part 2: $total2"
+puts "result $total1"
